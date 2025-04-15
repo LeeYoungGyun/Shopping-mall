155b2b7ddea1c573a811716a3a0ef1912fbe4d27
younggyun <noxium@naver.com>
2025-04-15 17:42:01 +0900

    feat: shop #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import MainPage  from './component/MainPage';
+import DetailPage from './component/DetailPage';
+import About from './component/About';
+import Event from './component/Event';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import backgroundImg from './assets/뽀로로.jpeg';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 function App() {
+  let navigate = useNavigate();  
 
   return (
     <div className='App'>
-      <Navbar bg="primary" data-bs-theme="dark" className='fixed-top'>
+      <Navbar bg="primary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link to='/'>홈</Link>
+            <Link to='/detail' style={{ marginLeft: '20px'}} >상세페이지</Link>
+            <Nav.Link onClick={ ()=> {navigate('/cart')}}>Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-
-      <div className='main-bg'></div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src={backgroundImg} alt="뽀로로" className="img-fluid" />
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className="col-md-4">안녕</div>
-          <div className="col-md-4">안녕</div>
-        </div>
-      </div> 
+      <Routes>
+        <Route path="/" element={ <MainPage />}/>
+        <Route path="/detail" element={ <DetailPage /> } />
+        <Route path="/about" element={ <About /> }>
+          <Route path="member" element={ <div>멤버 소개</div> } />
+          <Route path="location" element={ <div>위치 정보</div> } />
+        </Route>
+        <Route path="/event" element={ <Event /> }>
+          <Route path="one" element={ <div>첫 주문시 양배추즙 서비스</div> } />
+          <Route path="two" element={ <div>생일기념 쿠폰받기</div> } />
+        </Route>
+        <Route path="*" element={<div>Error 404</div>} />
+      </Routes>
     </div>
 
 
---
+++ src/component/MainPage.jsx
@@ -0,0 +1,14 @@
+import List from './List';
+
+const MainPage = () => {
+   return (
+   <div>
+      <div className='main-bg'></div>
+      <div className="container">
+       <List />
+      </div> 
+   </div>
+   );
+};
+
+export default MainPage;
